8be882be05846fb6904165924dc590ab37a80714
sonya <159024958+okawa11koto@users.noreply.github.com>
2026-02-21 12:32:24 +0300

    helpme

--- app/app.py
+++ app/app.py
@@ -1,49 +1,52 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from models import db, User
 from flask_caching import Cache
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['CACHE_TYPE'] = 'RedisCache'
-app.config['CACHE_REDIS_HOST'] = 'redis'
-app.config['CACHE_REDIS_PORT'] = 6379
-app.config['CACHE_DEFAULT_TIMEOUT'] = 300
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@db:5432/mydb'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['CACHE_TYPE'] = 'RedisCache'
+    app.config['CACHE_REDIS_HOST'] = 'redis'
+    app.config['CACHE_REDIS_PORT'] = 6379
+    app.config['CACHE_DEFAULT_TIMEOUT'] = 300
 
-db.init_app(app)
-cache = Cache(app)
+    db.init_app(app)
+    cache = Cache(app)
 
-@app.before_first_request
-def init_db():
-    db.create_all()
+    @app.before_serving
+    def init_db():
+        db.create_all()
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.json
-    user = User(name=data['name'])
-    db.session.add(user)
-    db.session.commit()
-    cache.clear()
-    return jsonify({'id': user.id, 'name': user.name})
+    @app.route('/users', methods=['POST'])
+    def create_user():
+        data = request.json
+        user = User(name=data['name'])
+        db.session.add(user)
+        db.session.commit()
+        cache.clear()
+        return jsonify({'id': user.id, 'name': user.name})
 
-@app.route('/users', methods=['GET'])
-@cache.cached(timeout=60)
-def get_users():
-    users = User.query.all()
-    return jsonify([{'id': u.id, 'name': u.name} for u in users])
+    @app.route('/users', methods=['GET'])
+    @cache.cached(timeout=60)
+    def get_users():
+        users = User.query.all()
+        return jsonify([{'id': u.id, 'name': u.name} for u in users])
 
-@app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
-    user = User.query.get_or_404(id)
-    user.name = request.json['name']
-    db.session.commit()
-    cache.clear()
-    return jsonify({'id': user.id, 'name': user.name})
+    @app.route('/users/<int:id>', methods=['PUT'])
+    def update_user(id):
+        user = User.query.get_or_404(id)
+        user.name = request.json['name']
+        db.session.commit()
+        cache.clear()
+        return jsonify({'id': user.id, 'name': user.name})
 
-@app.route('/users/<int:id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get_or_404(id)
-    db.session.delete(user)
-    db.session.commit()
-    cache.clear()
-    return '', 204
+    @app.route('/users/<int:id>', methods=['DELETE'])
+    def delete_user(id):
+        user = User.query.get_or_404(id)
+        db.session.delete(user)
+        db.session.commit()
+        cache.clear()
+        return '', 204
+
+    return app
--- app/models.py
+++ app/models.py
@@ -4,4 +4,4 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(50), nullable=False)
